c3f7092e2c14d3d945a93cc8daec2c11294dc3bd
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-01-25 12:22:51 -0700

    Documented mine and shorty's methods.

--- Pente/Pente/MainWindow.xaml.cs
+++ Pente/Pente/MainWindow.xaml.cs
@@ -140,6 +140,11 @@ namespace Pente
             InitializeComponent();
         }
         // B & E
+        /// <summary>
+        /// Once clicked, all players can enter their names and grid size.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event</param>
         public void PvP_Click(object sender, RoutedEventArgs e)
         {
             PlayerPanel.Visibility = Visibility.Collapsed;
@@ -148,6 +153,11 @@ namespace Pente
             computerEnabled = false;
         }
         // B & E
+        /// <summary>
+        /// Once clicked, player can enter their name and grid size.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event</param>
         public void PvC_Click(object sender, RoutedEventArgs e)
         {
             PlayerPanel.Visibility = Visibility.Collapsed;
@@ -194,6 +204,11 @@ namespace Pente
 
         }
         // B & E
+        /// <summary>
+        /// Once clicked, the game board is set and displayed.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event</param>
         public void Start_Click(object sender, RoutedEventArgs e)
         {
             PenteLabel.Visibility = Visibility.Collapsed;
@@ -489,7 +504,6 @@ namespace Pente
         }
 
         // G & M
-
         public bool CheckIfInBounds(int x, int y)
         {
             if (x < board.GetLowerBound(0) ||
@@ -645,6 +659,11 @@ namespace Pente
             NamePanel.Visibility = Visibility.Collapsed;
         }
         // B & E
+        /// <summary>
+        /// Once clicked, a dialog box opens to load a file.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event</param>
         public void LoadGame_Click(object sender, RoutedEventArgs e)
         {
             EndTimer();
@@ -730,6 +749,11 @@ namespace Pente
         }
 
         // B & E
+        /// <summary>
+        /// Once clicked, a dialog box opens to save a file.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event</param>
         public void SaveGame_Click(object sender, RoutedEventArgs e)
         {
             EndTimer();
@@ -770,7 +794,7 @@ namespace Pente
             return x > invalidMiddleSpot + 2 || x < invalidMiddleSpot - 2 ||
                    y > invalidMiddleSpot + 2 || y < invalidMiddleSpot - 2;
         }
-
+        // G & M
         private void BackBtn_Click(object sender, RoutedEventArgs e)
         {
             if(PlayBoardBackground.Visibility == Visibility.Visible)
@@ -786,7 +810,12 @@ namespace Pente
                 ResetVisibility();
             }
         }
-
+        // B & E
+        /// <summary>
+        /// Once clicked, the reset all variables and return to title screen.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event.</param>
         private void ResetGame_Click(object sender, RoutedEventArgs e)
         {
             ResetGame();
@@ -795,7 +824,12 @@ namespace Pente
             PenteLabel.Visibility = Visibility.Visible;
             PlayerPanel.Visibility = Visibility.Visible;
         }
-
+        // B & E
+        /// <summary>
+        /// Once clicked, the window will close.
+        /// </summary>
+        /// <param name="sender">Button element object</param>
+        /// <param name="e">Button event.</param>
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
